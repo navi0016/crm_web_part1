c7be81ea2a39eb9eed9de84c473c84dd76e1fdcc
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-23 19:13:18 -0400

    completed

--- crm.rb
+++ crm.rb
@@ -2,5 +2,24 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+    @contacts_page = '/contacts'
+    @about_me_page = '/about_me'
   erb :index
+
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  @home_page = '/'
+  erb :contacts
+end
+
+get '/about_me' do
+  @contacts_page = '/contacts'
+  @home_page = '/'
+  erb :about_me
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
